2f8698105c99d1e22c9171f02afe9eb0c8bf4286
javier deben <jdchacon99@gmail.com>
2023-06-07 16:49:47 +0100

    Changed logo adding Jumpn

--- db/migrate/20230605161350_create_subscriptions.rb
+++ db/migrate/20230605161350_create_subscriptions.rb
@@ -1,7 +1,6 @@
 class CreateSubscriptions < ActiveRecord::Migration[7.0]
   def change
     create_table :subscriptions do |t|
-      t.references :club, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.integer :price
 
--- db/migrate/20230605161520_create_activities.rb
+++ db/migrate/20230605161520_create_activities.rb
@@ -1,8 +1,6 @@
 class CreateActivities < ActiveRecord::Migration[7.0]
   def change
     create_table :activities do |t|
-      t.references :club, null: false, foreign_key: true
-
       t.timestamps
     end
   end
