16d8dfc7675f65bb4b59014f87277b5bc397c748
Louis Clermont <l.clermont12@gmail.com>
2023-06-14 14:38:22 +0100

    trainer form improvement

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -16,17 +16,37 @@ class ServicesController < ApplicationController
   def create
     @trainer = Trainer.find(params[:trainer_id])
     @service = Service.new(service_params)
+    @service.service_type = params[:service_type]
     @service.trainer = @trainer
-    if @service.save!
+    if @service.save
       redirect_to trainer_path(@trainer), notice: 'Service was successfully created.'
     else
       render :new
     end
   end
 
+  def edit
+    @service = Service.find(params[:id])
+  end
+
+  def update
+    @service = Service.find(params[:id])
+    if @service.update(service_params)
+      redirect_to trainer_path(@service.trainer), notice: 'Service was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @service = Service.find(params[:id])
+    @service.destroy
+    redirect_to trainer_service_path, notice: 'Service was successfully destroyed.'
+  end
+
   private
 
   def service_params
-    params.require(:service).permit(:trainer_id, :description, :service_type, :address, :sport, :price)
+    params.require(:service).permit(:trainer_id, :description, :service_type, :address, :sport, :price, :title, :duration)
   end
 end
