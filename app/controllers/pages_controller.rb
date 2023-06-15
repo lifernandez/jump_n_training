9c0f535d356412df1dc1bd29eabbd0749e0f6423
KEVIN MANLANGIT <kevinmanlangit24@gmail.com>
2023-06-15 14:08:03 +0100

    leave a review

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
   end
 
   def mybookings
+    @review = Review.new
     @bookings_session = []
     current_user.bookings.map do |booking|
       @bookings_session << booking if booking.service.service_type == 'Session'
@@ -13,6 +14,21 @@ class PagesController < ApplicationController
     @bookings_training = []
     current_user.bookings.map do |booking|
       @bookings_training << booking if booking.service.service_type == 'Training Plan'
+    end
+  end
+
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = @booking.build_review
+  end
+
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = @booking.build_review(review_params)
+    if @review.save
+      redirect_to @booking, notice: 'Review was successfully created.'
+    else
+      render :new
     end
   end
 
